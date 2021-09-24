2e7fa4740db564ec3c61295166dc650a18ea1c5a
Muhamad Yamin <72939609+azarein@users.noreply.github.com>
2021-09-24 00:52:41 -0700

    Add files via upload

---
+++ main/java/Jpaneltugas2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.*;
+/**
+ *
+ * @author Muhamad Yamin
+ */
+
+public class Jpaneltugas2 {
+    public static void main(String[] args){
+        String nama = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+nama+" Sangat mudah");        
+    }
+    
+}
---
+++ main/java/Tugas1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhamad Yamin
+ */
+public class Tugas1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
